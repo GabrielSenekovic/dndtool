b55fb8430323c33ec50838070f874cab02dfb893
Gabriel Senekovic <Gabriel.Senekovic@gmail.com>
2020-08-01 01:21:39 +0200

    UI and selection now exist and function properly. The UI still lacks buttons however, but that will be the next step

--- dndtool/dndtool.cpp
+++ dndtool/dndtool.cpp
@@ -45,6 +45,10 @@ class DnDTool : public olc::PixelGameEngine
 	};
 	struct token
 	{
+		token()
+		{
+
+		}
 		token(olc::Decal* icon_in, olc::vf2d position_in, olc::Pixel tint_in, std::string name_in)
 		{
 			icon = icon_in;
@@ -58,46 +62,64 @@ class DnDTool : public olc::PixelGameEngine
 		std::string name;
 	};
 
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//Here follows variables related to different interaction modes:
+	//MOVE
+	int selectedTile = -1; //if you right click on a tile, it becomes selected. This hold the index
+	//Also use this for measuring as the start point
+	token* heldToken = nullptr;
+	olc::vi2d previousTokenPosition = { 0,0 }; //in order to check if the heldtoken was the one that was selected
+	olc::Decal* selection = nullptr; //Decal that will show up behind selected tokens
+	float selectionAngle = 0; float selectionRotationSpeed = 0.01f; //Use this to rotate selection image
+	token selectedToken;
+	//DRAW
+	olc::Sprite* fogOfWarSprite = nullptr;
+	olc::Decal* fogOfWarDecal = nullptr; //Perhaps I only need to draw the sprite if I draw it on a higher layer
+	Gdiplus::Bitmap* eraserMask = nullptr;
+	//MEASURE
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	std::vector<map> maps;
 	std::vector<olc::Decal*> backgrounds;
-	olc::Sprite* fogOfWarSprite = nullptr;
-	olc::Decal* fogOfWarDecal = nullptr;
 	std::vector<olc::Decal*> icons;
-
 	std::vector<olc::Decal*> cursors;
 	std::vector<token> NPCs;
-	token* heldToken = nullptr;
-	olc::vf2d startPoint = { 0,0 };
 
 	olc::Decal* gridTile = nullptr;
 	int tileDivisor = 256;
 	int commonDivisorIndex = 8;
 	olc::Pixel gridColor = olc::WHITE;
+	float commonDivisor = 0;
 
 	Gdiplus::Bitmap* iconMask = nullptr;
-	Gdiplus::Bitmap* eraserMask = nullptr;
 
 	olc::vf2d zoom = { 1,1 };
+	olc::vf2d panOffset = { 0,0 }; //When zoomed in, used to drag the camera around, ie. pan around
 
 	olc::Decal* UIBorder = nullptr;
 	olc::vf2d UIoffset = { 0,0 }; //how offset the entire image is due to the UI
-
-	olc::vf2d selectedTile = { 0,0 };
+	float scaleUnaffectedByUI = 0;
+	float scaleAffectedByUI = 0;
 
 	int currentMap = 0;
+
 	public:
+		DnDTool(){}
 		float width = 1920.0f * 0.3f; //4
 		float height = 1080.0f * 0.3f;
+
 	bool OnUserCreate()override
 	{
 		LoadDecals();
 		LoadPlayers();
 		ConstructMaps();
+		ToggleUI();
 		return true;
 	}
 	void LoadDecals()
 	{
 		gridTile = new olc::Decal(new olc::Sprite("./Assets/Tile.png"));
+		selection = new olc::Decal(new olc::Sprite("./Assets/Selection.png"));
 		backgrounds.push_back(new olc::Decal(new olc::Sprite("./Assets/1.png")));
 		UIBorder = new olc::Decal(new olc::Sprite("./Assets/UI_Border.png"));
 
@@ -144,6 +166,7 @@ class DnDTool : public olc::PixelGameEngine
 		{
 			map("MapName", backgrounds[0], std::vector<map::link> { map::link("Hello", {5,5}) }, 1)
 		};
+		scaleUnaffectedByUI = maps[currentMap].background->sprite->width > maps[currentMap].background->sprite->height ? (width / (maps[currentMap].background->sprite->width)) : (height / maps[currentMap].background->sprite->height);
 	}
 	void FillFog(olc::Pixel color)
 	{
@@ -154,7 +177,16 @@ class DnDTool : public olc::PixelGameEngine
 	}
 	bool OnUserUpdate(float fElepsedTime)override
 	{
+		if (selectedTile > -1)
+		{
+			selectionAngle += selectionRotationSpeed;
+		}
 		DrawScene();
+		CheckInput();
+		return true;
+	}
+	void CheckInput()
+	{
 		if (GetMouse(0).bPressed)
 		{
 			switch (interactionMode)
@@ -165,7 +197,9 @@ class DnDTool : public olc::PixelGameEngine
 				}
 				case InteractionMode::MEASURE:
 				{
-					startPoint = GetMousePositionInXY();
+					//deselect by right clicking
+					int gridWidth = (maps[currentMap].background->sprite->width * (scaleUnaffectedByUI / commonDivisor));
+					selectedTile = GetMousePositionInXY().y * gridWidth + GetMousePositionInXY().x;
 				}
 			}
 		}
@@ -174,6 +208,33 @@ class DnDTool : public olc::PixelGameEngine
 			if (interactionMode == InteractionMode::DRAW)
 			{
 				EraseDraw(DrawMode::DRAWMODE_DRAW);
+			}
+		}
+		if (GetMouse(1).bPressed)
+		{
+			switch (interactionMode)
+			{
+				case InteractionMode::MOVE:
+				{
+					//Should only happen if youre pressing a token, otherwise it will deselect
+					int gridWidth = (maps[currentMap].background->sprite->width * (scaleUnaffectedByUI / commonDivisor));
+					selectionAngle = selectedTile == GetMousePositionInXY().y * gridWidth + GetMousePositionInXY().x ? selectionAngle : 0;
+					selectedTile = GetMousePositionInXY().y * gridWidth + GetMousePositionInXY().x;
+					for (int i = 0; i < NPCs.size(); i++)
+					{
+						if (NPCs[i].position.y * gridWidth + NPCs[i].position.x == selectedTile)
+						{
+							selectedToken = NPCs[i];
+						}
+					}
+					break;
+				}
+				case InteractionMode::MEASURE:
+				{
+					//Deselect measurement if you rightclick
+					selectedTile = -1;
+					break;
+				}
 			}
 		}
 		if (GetMouse(1).bHeld)
@@ -185,7 +246,7 @@ class DnDTool : public olc::PixelGameEngine
 		}
 		if (GetKey(olc::Q).bPressed)
 		{
-			UIoffset = { 32.0f * (UIoffset.x == 0) * width / UIBorder->sprite->width, 16.0f * (UIoffset.y == 0) * height / UIBorder->sprite->height };
+			ToggleUI();
 		}
 		if (GetKey(olc::W).bPressed)
 		{
@@ -211,7 +272,7 @@ class DnDTool : public olc::PixelGameEngine
 			}
 			if (GetKey(olc::N).bPressed)
 			{
-				commonDivisorIndex-= 1 * (commonDivisorIndex > 1);
+				commonDivisorIndex -= 1 * (commonDivisorIndex > 1);
 			}
 			if (GetMouse(0).bPressed && interactionMode == InteractionMode::DRAW)
 			{
@@ -228,7 +289,10 @@ class DnDTool : public olc::PixelGameEngine
 			zoom.x -= 0.001f;
 			zoom.y -= 0.001f;
 		}
-		return true;
+	}
+	void ToggleUI()
+	{
+		UIoffset = { 70.0f * (UIoffset.x == 0) * width / UIBorder->sprite->width, 16.0f * (UIoffset.y == 0) * height / UIBorder->sprite->height };
 	}
 	void EraseDraw(DrawMode mode)
 	{
@@ -268,9 +332,11 @@ class DnDTool : public olc::PixelGameEngine
 	}
 	olc::vi2d GetMousePositionInXY()
 	{
+		return olc::vi2d({(int) GetMousePositionInXYFloat().x, (int)GetMousePositionInXYFloat().y });
+	}
+	olc::vf2d GetMousePositionInXYFloat()
+	{
 		olc::vf2d scaleUIOffset = GetScaleUIOffset();
-		float scaleUnaffectedByUI = maps[currentMap].background->sprite->width > maps[currentMap].background->sprite->height ? (width / (maps[currentMap].background->sprite->width)) : (height / maps[currentMap].background->sprite->height);
-		float scaleAffectedByUI = maps[currentMap].background->sprite->width > maps[currentMap].background->sprite->height ? ((width - scaleUIOffset.x) / (maps[currentMap].background->sprite->width)) : ((height - scaleUIOffset.y) / maps[currentMap].background->sprite->height);
 		float commonDivisor = gnu::findCommonDivisors(maps[currentMap].background->sprite->width * scaleUnaffectedByUI, maps[currentMap].background->sprite->height * scaleUnaffectedByUI)[commonDivisorIndex];
 		float mapWidthScaledByUI = maps[currentMap].background->sprite->width * scaleAffectedByUI;
 		float tileableSize = commonDivisor / tileDivisor;
@@ -278,13 +344,12 @@ class DnDTool : public olc::PixelGameEngine
 		int gridWidth = (maps[currentMap].background->sprite->width * amountOfColumns);
 		float adjustedTileWidth = (mapWidthScaledByUI / gridWidth) / tileableSize;
 
-		olc::vf2d downScaledTileDimensions = { commonDivisor * adjustedTileWidth, commonDivisor * adjustedTileWidth};
+		olc::vf2d downScaledTileDimensions = { commonDivisor * adjustedTileWidth, commonDivisor * adjustedTileWidth };
 
-		return olc::vi2d({(int) std::floor((GetMouseX()-UIoffset.x) * tileDivisor /downScaledTileDimensions.x ), (int)std::floor((GetMouseY()-UIoffset.y) * tileDivisor / downScaledTileDimensions.y) });
+		return olc::vf2d({(float)(GetMouseX() - UIoffset.x) * (float)tileDivisor / downScaledTileDimensions.x, (float)(GetMouseY() - UIoffset.y) * (float)tileDivisor / downScaledTileDimensions.y });
 	}
 	void PickUpToken()
 	{
-		float commonDivisor = gnu::findCommonDivisors(width, height)[commonDivisorIndex];
 		olc::vf2d MousePositionInXY = GetMousePositionInXY();
 		for (int i = 0; i < NPCs.size(); i++)
 		{
@@ -294,6 +359,7 @@ class DnDTool : public olc::PixelGameEngine
 				if (heldToken == nullptr)
 				{
 					heldToken = &NPCs[i];
+					previousTokenPosition = NPCs[i].position;
 					NPCs[i].position = { -1,-1 }; //Instead of making the icon invisible, I move it offscreen
 				}
 				else
@@ -307,7 +373,13 @@ class DnDTool : public olc::PixelGameEngine
 		//nothing is on this tile
 		if (heldToken != nullptr)
 		{
+			int gridWidth = (maps[currentMap].background->sprite->width * (scaleUnaffectedByUI / commonDivisor));
+			if (selectedTile == previousTokenPosition.y * gridWidth + previousTokenPosition.x)
+			{
+				selectedTile = GetMousePositionInXY().y * gridWidth + GetMousePositionInXY().x;
+			}
 		    heldToken->position = { MousePositionInXY.x,MousePositionInXY.y };
+
 			heldToken = nullptr;
 		}
 	}
@@ -316,35 +388,32 @@ class DnDTool : public olc::PixelGameEngine
 		Clear(olc::BLACK);
 		olc::vf2d scaleUIOffset = UIoffset.x == 0 ? olc::vf2d{ 0,0 } : olc::vf2d{ 48 * width / UIBorder->sprite->width, 32 * height / UIBorder->sprite->height };
 		
-		float scaleUnaffectedByUI = maps[currentMap].background->sprite->width > maps[currentMap].background->sprite->height ? (width/ (maps[currentMap].background->sprite->width)) : (height / maps[currentMap].background->sprite->height);
-		float scaleAffectedByUI = maps[currentMap].background->sprite->width > maps[currentMap].background->sprite->height ? ((width - scaleUIOffset.x) / (maps[currentMap].background->sprite->width)) : ((height - scaleUIOffset.y) / maps[currentMap].background->sprite->height);
+		scaleAffectedByUI = maps[currentMap].background->sprite->width > maps[currentMap].background->sprite->height ? ((width - scaleUIOffset.x) / (maps[currentMap].background->sprite->width)) : ((height - scaleUIOffset.y) / maps[currentMap].background->sprite->height);
 		
 		DrawDecal({ UIoffset.x,UIoffset.y }, maps[currentMap].background, { scaleAffectedByUI* zoom.x,scaleAffectedByUI* zoom.y});
 		float scaledWidth = maps[currentMap].background->sprite->width * scaleAffectedByUI * zoom.x;
-		float commonDivisor = gnu::findCommonDivisors(maps[currentMap].background->sprite->width * scaleUnaffectedByUI, maps[currentMap].background->sprite->height * scaleUnaffectedByUI)[commonDivisorIndex];
-		olc::vf2d MousePositionInXY = GetMousePositionInXY();
+		commonDivisor = gnu::findCommonDivisors(maps[currentMap].background->sprite->width * scaleUnaffectedByUI, maps[currentMap].background->sprite->height * scaleUnaffectedByUI)[commonDivisorIndex];
 		
 		float tileableSize = commonDivisor / tileDivisor;
 		float amountOfColumns = scaleUnaffectedByUI / commonDivisor;
-		int gridWidth = (maps[currentMap].background->sprite->width * amountOfColumns);
+		int gridWidth = maps[currentMap].background->sprite->width * amountOfColumns;
 		float adjustedTileWidth = ((maps[currentMap].background->sprite->width * scaleAffectedByUI) / gridWidth) / tileableSize;
 		float tileWidthRatio = gridTile->sprite->width / adjustedTileWidth;
 		if (interactionMode == InteractionMode::MOVE || interactionMode == InteractionMode::MEASURE)
 		{
-			DrawGrid(commonDivisor, MousePositionInXY, scaleUnaffectedByUI, tileWidthRatio, tileableSize, gridWidth);
+			DrawGrid(commonDivisor, scaleUnaffectedByUI, tileWidthRatio, tileableSize, gridWidth);
 		}
-		DrawTokens(commonDivisor, MousePositionInXY, tileWidthRatio);
-		DrawLinks(commonDivisor, MousePositionInXY);
+		DrawTokens(tileWidthRatio);
+		DrawLinks(commonDivisor);
 		DisplayFog();
 		if (UIoffset.x > 0)
 		{
-			DrawDecal({ 0,0 }, UIBorder, {width/UIBorder->sprite->width, height/UIBorder->sprite->height });
-			DisplayHUD(MousePositionInXY);
+			DisplayHUD();
 		}
-		DrawCursor(commonDivisor, MousePositionInXY);
+		DrawCursor(commonDivisor);
 	}
 
-	void DrawGrid(float commonDivisor, olc::vf2d mousePositionInXY, float scale, float tileWidthRatio, float tileableSize, int gridWidth)
+	void DrawGrid(float commonDivisor, float scale, float tileWidthRatio, float tileableSize, int gridWidth)
 	{
 		olc::vf2d tileScale = { tileableSize * zoom.x / tileWidthRatio, tileableSize * zoom.y / tileWidthRatio };
 
@@ -352,19 +421,30 @@ class DnDTool : public olc::PixelGameEngine
 		{
 			int x = i % gridWidth; 
 			int y = i / gridWidth;
-			bool isThisHighlighted = ((mousePositionInXY.x == x && (mousePositionInXY.y == y)));
+			bool isThisHighlighted = ((GetMousePositionInXY().x == x && (GetMousePositionInXY().y == y)));
 			olc::Pixel tint = isThisHighlighted ? olc::RED : gridColor;
+			olc::vf2d adjustedDimensions = { gridTile->sprite->width * tileableSize * zoom.x / tileWidthRatio , gridTile->sprite->height * tileableSize * zoom.y / tileWidthRatio };
+			olc::vf2d position = {
+				(float)x * adjustedDimensions.x + UIoffset.x,
+				(float)y * adjustedDimensions.y + UIoffset.y };
 
-			DrawDecal({
-				(float)x * gridTile->sprite->width * tileableSize * zoom.x / tileWidthRatio + UIoffset.x,
-				(float)y * gridTile->sprite->height * tileableSize * zoom.y / tileWidthRatio + UIoffset.y},
-				gridTile, tileScale, tint);
+			if (i == selectedTile)
+			{
+				float centeringModifier = (gridTile->sprite->width * tileableSize) - ((gridTile->sprite->width * tileableSize)) / 2;
+				float iconToTileRatio = (float)gridTile->sprite->width / (float)NPCs[0].icon->sprite->width;
+				olc::vf2d scale = { tileableSize * zoom.x * iconToTileRatio / tileWidthRatio, tileableSize * zoom.y * iconToTileRatio / tileWidthRatio };
+				olc::vf2d centerOfRotation = { (float)selection->sprite->width/2 , (float)selection->sprite->height/2 };
+				olc::vf2d selectionPosition = { (x * gridTile->sprite->width * tileableSize + centeringModifier) * zoom.x / tileWidthRatio + UIoffset.x, (y * gridTile->sprite->height * tileableSize + centeringModifier) * zoom.y / tileWidthRatio + UIoffset.y };
+
+				DrawRotatedDecal(selectionPosition, selection, selectionAngle, centerOfRotation, scale, olc::RED);
+			}
+			DrawDecal(position,gridTile, tileScale, tint);
 		}
 
 		olc::vf2d cursorPosition = { GetMouseX() * zoom.x, GetMouseY() * zoom.y };
-		//DrawDecal(cursorPosition, gridTile, tileScale, olc::BLUE);
+		DrawDecal(cursorPosition, gridTile, tileScale, olc::BLUE);
 	}
-	void DrawTokens(float commonDivisor, olc::vf2d mousePosition, float tileWidthRatio)
+	void DrawTokens(float tileWidthRatio)
 	{
 		for (unsigned int i = 0; i < NPCs.size(); i++)
 		{
@@ -378,17 +458,21 @@ class DnDTool : public olc::PixelGameEngine
 			olc::vf2d position = {( NPCs[i].position.x * gridTile->sprite->width * tileableSize + centeringModifier)*zoom.x/tileWidthRatio + UIoffset.x, (NPCs[i].position.y * gridTile->sprite->height * tileableSize + centeringModifier)*zoom.y/tileWidthRatio + UIoffset.y};
 			
 			DrawDecal(position, NPCs[i].icon, scale, NPCs[i].tint);
-			if (mousePosition.x == NPCs[i].position.x && mousePosition.y == NPCs[i].position.y)
+			float gridWidth = (maps[currentMap].background->sprite->width * (scaleUnaffectedByUI / commonDivisor));
+			bool isSelected = selectedTile == (int)(NPCs[i].position.y * gridWidth + NPCs[i].position.x);
+
+			if (GetMousePositionInXY().x == NPCs[i].position.x && GetMousePositionInXY().y == NPCs[i].position.y || isSelected)
 			{
 				//if this NPC is highlighted
 				float lengthOfName =NPCs[i].name.length() * 8;
-
+				olc::Pixel tint = isSelected ? olc::RED : olc::WHITE;
+				float yPositionModifier = isSelected ? 24 : 20;
 				float lengthToSubtract = ((lengthOfName - gridTile->sprite->width)/4) + gridTile->sprite->width*tileableSize*2;
-				DrawStringDecal({ position.x - lengthToSubtract * zoom.x, position.y + 20 * zoom.y / tileWidthRatio}, NPCs[i].name, olc::WHITE, {0.5f * zoom.x, 0.5f * zoom.y });
+				DrawStringDecal({ position.x - lengthToSubtract * zoom.x, position.y + yPositionModifier * zoom.y / tileWidthRatio}, NPCs[i].name, tint, {0.5f * zoom.x, 0.5f * zoom.y });
 			}
 		}
 	}
-	void DrawLinks(float modifier, olc::vf2d mousePosition)
+	void DrawLinks(float modifier)
 	{
 		for (unsigned int i = 0; i < maps[currentMap].links.size(); i++)
 		{
@@ -399,14 +483,14 @@ class DnDTool : public olc::PixelGameEngine
 			DrawStringDecal(position, maps[currentMap].links[i].mapIdentifier, tint, { 1, 1 });
 		}
 	}
-	void DrawCursor(float modifier, olc::vf2d mousePosition)
+	void DrawCursor(float modifier)
 	{
 		bool grabbable = false;
 		olc::vf2d scale = { 0.5f,0.5f };
 		olc::vf2d position = { (GetMouseX()-10) * zoom.x, (GetMouseY()-10) * zoom.y };
 		for (size_t i = 0; i < NPCs.size(); i++)
 		{
-			if (NPCs[i].position.x == mousePosition.x && NPCs[i].position.y == mousePosition.y)
+			if (NPCs[i].position.x == GetMousePositionInXY().x && NPCs[i].position.y == GetMousePositionInXY().y)
 			{
 				grabbable = true;
 			}
@@ -447,13 +531,24 @@ class DnDTool : public olc::PixelGameEngine
 		fogOfWarDecal = new olc::Decal(fogOfWarSprite);
 		DrawDecal({ 0,0 }, fogOfWarDecal);
 	}
-	void DisplayHUD(olc::vi2d mousePosition)
+	void DisplayHUD()
 	{
-		DrawStringDecal({ 24,2 }, 
-			"[Grid Coord X: " + std::to_string(mousePosition.x) + " Y: " + std::to_string(mousePosition.y) + "]"+
-			" [Zoom:" + std::to_string(zoom.x) + "]"
+		DrawDecal({ 0,0 }, UIBorder, { width / UIBorder->sprite->width, height / UIBorder->sprite->height });
+		if (selectedToken.icon != nullptr)
+		{
+			float ratio = (62.0f/selectedToken.icon->sprite->width) * width/UIBorder->sprite->width;
+			DrawDecal({3,12 }, selectedToken.icon, {ratio,ratio});
+			DrawStringDecal({ 3, 64 }, selectedToken.name,olc::WHITE, {0.5f, 0.5f});
+		}
+		DrawStringDecal({ 2,2 }, 
+			"[Grid Coord X: " + std::to_string(GetMousePositionInXY().x) + " Y: " + std::to_string(GetMousePositionInXY().y) + "]"+
+			"[F_Grid Coord X: " + std::to_string(GetMousePositionInXYFloat().x) + " Y: " + std::to_string(GetMousePositionInXYFloat().y) + "]" +
+			" [Selected tile Index: " + std::to_string(selectedTile) +
+			" [Selected tile X: " + std::to_string(selectedTile%(int)(maps[currentMap].background->sprite->width * (scaleUnaffectedByUI / commonDivisor))) + " Y: " + std::to_string((int)(selectedTile / (maps[currentMap].background->sprite->width * (scaleUnaffectedByUI / commonDivisor))))
 			,olc::WHITE, { 0.5f, 0.5f });
-		DrawStringDecal({ 24,7 }, "[Mouse X: " + std::to_string(GetMouseX()) + " Y: " + std::to_string(GetMouseY()) + "]", olc::WHITE, { 0.5f, 0.5f });
+		DrawStringDecal({ 2,7 }, "[Mouse X: " + std::to_string(GetMouseX()) + " Y: " + std::to_string(GetMouseY()) + "]" +
+			" [Zoom:" + std::to_string(zoom.x) + "]"
+			, olc::WHITE, { 0.5f, 0.5f });
 	}
 	void Quit()
 	{
